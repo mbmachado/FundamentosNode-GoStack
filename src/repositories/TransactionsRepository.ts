6cba0e7e27d3e6da4487ba9686f373c938208694
Mateus Barbosa <mbmachado@outlook.com>
2020-07-09 15:43:35 -0300

    completes missing code

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const defaultBalance: Balance = { income: 0, outcome: 0, total: 0 };
+    return this.transactions.reduce((total, current) => {
+      if (current.type === 'income') {
+        return {
+          income: total.income + current.value,
+          outcome: total.outcome,
+          total: total.income + current.value,
+        };
+      }
+
+      return {
+        income: total.income,
+        outcome: total.outcome + current.value,
+        total: total.income - current.value,
+      };
+    }, defaultBalance);
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
